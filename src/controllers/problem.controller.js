4790ebfee7f3143373cd41d48519254a63dff12d
sharthak singh <sharthaksingh06@gmail.com>
2025-06-07 00:34:38 +0530

    Feat: Added routing structure for version api for the problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,34 @@
+function pingProblemController(req,res){
+    return res.json({message:'Ping Controller is up'});
+}
+
+function addProblem(req,res) {
+
+}
+
+function getProblem(req,res){
+
+}
+
+function getProblems(req,res){
+
+}
+
+function deleteProblem(req,res){
+
+}
+
+function updateProblem(req,res){
+
+}
+
+
+module.exports = {
+    addProblem,
+    getProblem,
+    getProblems,
+    deleteProblem,
+    updateProblem,
+    pingProblemController
+
+}
