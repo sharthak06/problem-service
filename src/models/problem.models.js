fe3c0886588a31baf6086a214fbdd3c1970adc25
sharthak singh <sharthaksingh06@gmail.com>
2025-07-05 01:28:55 +0530

    Feat: Prepared Schema for problems

---
+++ src/models/problem.models.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const problemSchema = new mongoose.Sdhema({
+    title:{
+        type: String,
+        required : [true,'Title cannot be empty']
+    },
+    description : {
+        type: String,
+        required : [true,'Description cannot be empty']
+    },
+    difficulty : {
+        type: String,
+        enum: ['easy','medium','hard'],
+        required : [true,'Difficulty cannot be empty'],
+        default :'easy'
+    },
+     testCases: [
+        {
+            input: {
+                type: String,
+                required: true
+            },
+            output: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    editorial: {
+        type: String
+    }
+
+
+});
+
+const Problem = mongoose.model('Problem', problemSchema);
+
+module.exports = Problem;
